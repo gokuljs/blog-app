aa52ff6a5d728834e40aa510c408ad4eba3a94c2
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-08-06 20:18:19 +0530

    new.ejs

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 console.log(__dirname) // gives the dir name which the project is currently working on 
 app.set('view engine', 'ejs');
-
+// home page 
 app.get("/", function(req, res) {
     res.render("home")
 });
+// blog page which shows list of all blogs 
 app.get("/blogs", function(req, res) {
     res.render("index")
+});
+// creating one form to make new blogs
+app.get("/blogs/new", function(req, res) {
+    res.render("new")
 })
-
 
 app.listen(4000, function(req, res) {
     console.log("port has started ")
